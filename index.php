ee69b643d7ebc8cdeac9997720d5bfcf792ea50a
Julian van Husen <210549@student.glu.nl>
2021-11-11 12:27:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,18 +2,20 @@
 
 get_header();
 
-if (have_posts()) :
-	while (have_posts()) : the_post();
-	
-	get_template_part('content');
-	
-	endwhile;
-	
-	else :
-		echo '<p>No content found</p>';
-		
-	endif;
+if(have_posts()) : while(have_posts()) : the_post() ?>
+            <?php the_title('<h1>','</h1>'); ?>
+            <?php if ( is_single() && 'post' == get_post_type() ): ?>
+                <?php the_content(); ?>
+                <?php if (comments_open() || get_comments_number() ): ?> 
+                    <?php comments_template(); ?>
+                <?php endif; ?>                     
+            <?php else: ?>
+                <?php the_excerpt(); ?>
+                <a href="<?php the_permalink(); ?>">lees meer</a>
+            <?php endif; ?>
+        <?php endwhile; endif;
 	
 get_footer();
 
 ?>
+
